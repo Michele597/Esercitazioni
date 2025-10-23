e3c9596e19b7822da3a4aa64aebfc19bfed4babd
Michele597 <micheleruggiero597@gmail.com>
2025-10-23 15:03:15 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $background = "background-color: black; text-align: center; padding: 20px;";
-
-  $testo = "color: white; font-weight: bold; font-family: Arial, sans-serif; margin: 0;";
-
-   echo "<footer style='$background'> <h1 style='$testo'></h1> </header>";
-?>
